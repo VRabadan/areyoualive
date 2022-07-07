0219b3ee602ba3f4f2d7b2ef00992789e1e571a8
VRabadan <vrg.phoenix@gmail.com>
2022-07-07 16:15:25 +0200

    Removed hardcoded names and environments

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const axios = require('axios');
+const { version } = require('prettier');
 const SlackWebhook = require('slack-webhook');
+const environments = require('./config.json');
 require('dotenv').config();
 
-const Slack = new SlackWebhook(process.env.SLACK_WEBHOOK_URL);
-const email = process.env.USER_EMAIL;
-const secret = process.env.USER_SECRET;
-const app = process.env.APP_URL;
+const slack = new SlackWebhook(process.env.SLACK_WEBHOOK_URL);
+
+const init = {
+  "blocks": [
+    {
+      "type": "section",
+      "text": {
+        "type": "mrkdwn",
+        "text": `${process.env.APP_NAME}`
+      }
+    },
+    {
+      "type": "divider"
+    },
+  ]
+};
 
 async function login(user, pw, url) {
   const request = {
@@ -19,12 +33,28 @@ async function login(user, pw, url) {
   let response;
   try {
     response = await axios(request);
+    return response.data;
   } catch (error) {
     console.log(error);
-    response = error.response;
+    if (error.code === 'ECONNREFUSED' || error.code === 'ECONNRESET') {
+      response = {
+        Login: {
+          message: error.syscall,
+          status: error.errno,
+          version: 'N/A'
+        }
+      };
+    } else {
+      response = {
+        Login: {
+          message: error.response.statusText,
+          status: error.response.status,
+          version: 'N/A'
+        }
+      };
+      return response;
+    }
   }
-
-  return response.data;
 }
 
 async function areYouAlive(token, url) {
@@ -38,129 +68,91 @@ async function areYouAlive(token, url) {
   let response;
   try {
     response = await axios(request);
+    return response.data;
   } catch (error) {
     console.log(error);
-    response = error.response;
-  }
-  return response.data;
-}
-
-function pretify(message) {
-  const string = JSON.stringify(message);
-  return string;
-}
-
-function statuslinter(key, value){
-  if(key === 'status'){
-    if(value === 200 ){
-      return ':white_check_mark:';
+    if (error.code === 'ECONNREFUSED' || error.code === 'ECONNRESET') {
+      response = {
+        Heartbeat: {
+          message: error.syscall,
+          status: error.errno,
+          version: 'N/A'
+        }
+      };
+    } else {
+      response = {
+        Heartbeat: {
+          message: error.response.statusText,
+          status: error.response.status,
+          version: 'N/A'
+        }
+      };
+      return response;
     }
+  }
+}
+
+function formatMessage(key, value) {
+  if (value[key].status >= 200 && value[key].status < 400) {
+    return `:white_check_mark: ${value[key].message} Version ${value[key].version} \n`;
+  } else {
+    return `:x: ${key} Error ${value[key].status}: ${value[key].message} Version ${value[key].version} \n`;
+  }
+}
+
+function statuslinter(value) {
+  const keys = Object.keys(value);
+  const message = keys.reduce((p, c) => {
+    if (Object.keys(value[c]).length > 0) {
+      const formatedMessage = value[c].status ? formatMessage(c, value) : statuslinter(value[c]);
+      p += formatedMessage;
+      return p;
+    }
+    return p;
+  }, '');
+  return message;
+}
+
+function concatenateEnv(env, data, message) {
+  const newenv = {
+    "type": "section",
+    "text": {
+      "type": "mrkdwn",
+      "text": `*${env}*\n${statuslinter(data)}`
+    }
+  };
+
+  message.blocks.push(newenv);
+  return message;
 }
 
 (async () => {
-const token = await login(email, secret, app);
-const isItAlive = await areYouAlive(token, app);
-console.log(isItAlive);
-const text = pretify(isItAlive);
-const env = "Application Testing";
-const test = {
-	"blocks": [
-		{
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
-				"text": `FCA Status for ${env}`
-			}
-		},
-		{
-			"type": "divider"
-		},
-		{
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
-				"text": `*FCA Backend*\n${JSON.stringify(isItAlive.Backend, null, 4)}`
-			}
-		},
-    {
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
-				"text": `*FCA Analysis*\n${JSON.stringify(isItAlive.Analysis, null, 4)}`
-			}
-		},
-    {
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
-				"text": `*FCA Identity*\n${JSON.stringify(isItAlive.Identity, null, 4)}`
-			}
-		},
-    {
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
-				"text": `*FCA Storage*\n${JSON.stringify(isItAlive.Storage.StorageAPI, null, 4)}`
-			}
-		},
-		{
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
-				"text": `*FCA Anonymization*\n${JSON.stringify(isItAlive.Storage.AnonymizationAPI, null, 4)}`
-			}
-		},
-		{
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
-				"text": `*FCA Recommender*\n${JSON.stringify(isItAlive.Recommender, null, 4)}`
-			}
-		},
-		{
-			"type": "divider"
-		},
-		{
-			"type": "actions",
-			"elements": [
-				{
-					"type": "button",
-					"text": {
-						"type": "plain_text",
-						"text": "Deploy",
-						"emoji": true
-					},
-					"value": "click_me_123"
-				},
-				{
-					"type": "button",
-					"text": {
-						"type": "plain_text",
-						"text": "Kill",
-						"emoji": true
-					},
-					"value": "click_me_123",
-					"url": "https://google.com"
-				},
-				{
-					"type": "button",
-					"text": {
-						"type": "plain_text",
-						"text": "Order Pizza",
-						"emoji": true
-					},
-					"value": "click_me_123",
-					"url": "https://google.com"
-				}
-			]
-		}
-	]
-};
 
-try {
-  Slack.send(test);
-} catch (error) {
-  console.log(error);
-}
-
+  const message = init;
+  for (const env in environments) {
+    const token = await login(environments[env].mail, environments[env].secret, environments[env].url);
+    if (!token) {
+      console.log(`${env} - Login Error`);
+      concatenateEnv(env, token, message);
+    } else {
+      const data = await areYouAlive(token, environments[env].url);
+      if (environments[env].skip) {
+        for (const app of environments[env].skip) {
+          const keys = app.split('.');
+          if(keys.length > 1) {
+            delete data[keys[0]][keys[1]];
+          } else {
+            delete data[app];
+          }
+        }
+      }
+      concatenateEnv(env, data, message);
+    }
+  }
+  try {
+    meta = slack.send(message);
+    console.log(meta);
+  } catch (error) {
+    console.log(error);
+  }
 })();
